cdf6d552cdfabb164af621d61423cd4fbd496c8c
Dmitry Skibitsky <dmitryskibitsky@gmail.com>
2025-06-05 01:24:18 +0300

    chore: install Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,7 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import { invoke } from '@tauri-apps/api/core';
-
-const greetMsg = ref('');
-const name = ref('');
-
-async function greet() {
-  greetMsg.value = await invoke('greet', { name: name.value });
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <MyComponent
-    id=""
-    ref=""
-    style=""
-    bar="2"
-    baz="3"
-    foo="1"
-    @click=""
-  />
-
-  <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
-
-    <form
-      class="row"
-      @submit.prevent="greet"
-    >
-      <input
-        id="greet-input"
-        v-model="name"
-        placeholder="Enter a name..."
-      />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
-  </main>
+  <v-card text="test" variant="tonal" @click="() => {}"></v-card>
 </template>
 
 <style scoped></style>
